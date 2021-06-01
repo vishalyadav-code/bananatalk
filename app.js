be230931edb839adcec2819b6c781b4c6e036312
vishalyadav-code <vishalyadav23398@gmail.com>
2021-06-01 18:02:42 +0530

    banana

---
+++ app.js
@@ -0,0 +1,24 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+function getTranslationURL(input){
+return serverURL + "?" + "text=" + input
+}
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server! try again after sometime")
+}
+
+function clickHandler() {
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
